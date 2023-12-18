5fba80ae5e704a83113930685aabb2450befd464
Grant Oxer <grant.oxer@gmail.com>
2023-12-18 14:42:29 -0800

    replaced Hello World with Table component and created Feature component

---
+++ web-app/src/app/Feature.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Feature(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Table from './Table'
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
-  )
+    <Table />
+  );
 }
