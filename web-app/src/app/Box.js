dd8c29666c3ea25aa756d1ec94c0e6b0fb8bece7
Grant Oxer <grant.oxer@gmail.com>
2023-12-21 10:05:57 -0800

    created Box component

---
+++ web-app/src/app/Box.js
@@ -0,0 +1,25 @@
+'use client'
+
+import React from "react";
+
+export default function Box(props) {
+    function handleClick() {
+        if (props.canReceiveFrom.includes(props.ball1FeatureId)) {
+            const ball = document.getElementById('ball');
+            const correspondingFeature = document.getElementById(props.correspondingFeatureId);
+            ball.style.left = correspondingFeature.style.left;
+            ball.style.top = correspondingFeature.style.top;
+            props.setBall1FeatureId(correspondingFeature.id);
+        } else {
+            alert("invalid choice!");
+        }
+    }
+
+    return (
+        <div id={props.featureId}
+            onClick={handleClick}
+            style={{position: "absolute", top: props.y, left: props.x, height: props.height, width: props.width}}
+        >
+        </div>
+    )
+}
