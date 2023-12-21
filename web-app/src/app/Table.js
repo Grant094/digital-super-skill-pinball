79d7750b48f588b7a2a550def763a656a3b9d533
Grant Oxer <grant.oxer@gmail.com>
2023-12-21 10:28:21 -0800

    removed canReceiveFrom from flippers since they do not receive the ball directly

--- web-app/src/app/Table.js
+++ web-app/src/app/Table.js
@@ -229,18 +229,6 @@ export default function Table(props) {
                 width="45px"
             />
             <Feature featureId="red-flipper"
-                canReceiveFrom={[
-                    'red-flipper',
-                    'yel-flipper',
-                    'droptarget-yel-12',
-                    'droptarget-yel-34',
-                    'droptarget-yel-56',
-                    'droptarget-red-12',
-                    'droptarget-red-3',
-                    'droptarget-red-4',
-                    'droptarget-red-56',
-                    'start'
-                ]}
                 ball1FeatureId={ball1FeatureId}
                 ball2FeatureId={ball2FeatureId}
                 setBall1FeatureId={setBall1FeatureId}
@@ -251,18 +239,6 @@ export default function Table(props) {
                 width="25px"
             />
             <Feature featureId="yel-flipper"
-                canReceiveFrom={[
-                    'red-flipper',
-                    'yel-flipper',
-                    'droptarget-yel-12',
-                    'droptarget-yel-34',
-                    'droptarget-yel-56',
-                    'droptarget-red-12',
-                    'droptarget-red-3',
-                    'droptarget-red-4',
-                    'droptarget-red-56',
-                    'start'
-                ]}
                 ball1FeatureId={ball1FeatureId}
                 ball2FeatureId={ball2FeatureId}
                 setBall1FeatureId={setBall1FeatureId}
