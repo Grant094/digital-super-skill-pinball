d6aa284dda1a8d51d43f7564dba0b79b5b196352
Grant Oxer <grant.oxer@gmail.com>
2024-01-08 10:48:53 -0800

    created DiceTray with randomly-rolled dice and added it to page.js

--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -2,11 +2,13 @@
 
 import styles from './page.module.css'
 import Table from './Table'
+import DiceTray from './DiceTray';
 
 export default function Home() {
   return (
     <div>
       <Table />
+      <DiceTray />
     </div>
   );
 }
