d4e9cdc95af67a977b7a0e657db5d94e20e23385
Grant Oxer <grant.oxer@gmail.com>
2024-01-08 11:10:33 -0800

    changed how dice were stored and passed to align with react framework

--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -1,14 +1,28 @@
 'use client'
 
+import React, { useState } from "react";
+import getRndIntegerInclusive from "./getRndIntegerInclusive";
 import styles from './page.module.css'
 import Table from './Table'
 import DiceTray from './DiceTray';
 
 export default function Home() {
+  const [die1, setDie1] = useState(0);
+  const [die2, setDie2] = useState(0);
+
+  function rollDice() {
+    setDie1(getRndIntegerInclusive(1, 6));
+    setDie2(getRndIntegerInclusive(1, 6));
+  }
+
   return (
     <div>
       <Table />
-      <DiceTray />
+      <DiceTray
+        die1={die1}
+        die2={die2}
+        rollDice={rollDice}
+      />
     </div>
   );
 }
