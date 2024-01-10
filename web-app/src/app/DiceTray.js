f52028c4b36ce3a0855fdbe13552c7c6d0b14314
Grant Oxer <grant.oxer@gmail.com>
2024-01-09 17:26:03 -0800

    moved buttonIds for nudge buttons to be on the same line of code as the component name

--- web-app/src/app/DiceTray.js
+++ web-app/src/app/DiceTray.js
@@ -17,32 +17,28 @@ export default function DiceTray(props) {
         >
             <p className={styles.Die1}>{props.die1}</p>
             <p className={styles.Die2}>{props.die2}</p>
-            <NudgeUpButton
-                buttonId="nudge-up-button-die1"
+            <NudgeUpButton buttonId="nudge-up-button-die1"
                 die={props.die1}
                 dieSetter={props.setDie1}
                 otherDieAmountNudgedBy={props.die2AmountNudgedBy}
                 thisDieAmountNudgedBy={props.die1AmountNudgedBy}
                 setThisDieAmountNudgedBy={props.setDie1AmountNudgedBy}
             />
-            <NudgeDnButton
-                buttonId="nudge-dn-button-die1"
+            <NudgeDnButton buttonId="nudge-dn-button-die1"
                 die={props.die1}
                 dieSetter={props.setDie1}
                 otherDieAmountNudgedBy={props.die2AmountNudgedBy}
                 thisDieAmountNudgedBy={props.die1AmountNudgedBy}
                 setThisDieAmountNudgedBy={props.setDie1AmountNudgedBy}
             />
-            <NudgeUpButton
-                buttonId="nudge-up-button-die2"
+            <NudgeUpButton buttonId="nudge-up-button-die2"
                 die={props.die2}
                 dieSetter={props.setDie2}
                 otherDieAmountNudgedBy={props.die1AmountNudgedBy}
                 thisDieAmountNudgedBy={props.die2AmountNudgedBy}
                 setThisDieAmountNudgedBy={props.setDie2AmountNudgedBy}
             />
-            <NudgeDnButton
-                buttonId="nudge-dn-button-die2"
+            <NudgeDnButton buttonId="nudge-dn-button-die2"
                 die={props.die2}
                 dieSetter={props.setDie2}
                 otherDieAmountNudgedBy={props.die1AmountNudgedBy}
