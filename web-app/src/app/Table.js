ccb8d557dc9226607684ac37a271eefb4c25b6cf
Grant Oxer <grant.oxer@gmail.com>
2024-01-12 10:54:13 -0800

    added feat of strength features and boxes

--- web-app/src/app/Table.js
+++ web-app/src/app/Table.js
@@ -19,6 +19,22 @@ export default function Table(props) {
         "feature-ferriswheel-car-34",
         "feature-ferriswheel-car-56",
     ];
+    const featofstrengthFeatureIds = [
+        "feature-featofstrength-1",
+        "feature-featofstrength-2",
+        "feature-featofstrength-3",
+        "feature-featofstrength-4",
+        "feature-featofstrength-5",
+        "feature-featofstrength-6",
+    ];
+    const featofstrengthGroupBoxIds = [
+        "box-featofstrength-1",
+        "box-featofstrength-2",
+        "box-featofstrength-3",
+        "box-featofstrength-4",
+        "box-featofstrength-5",
+        "box-featofstrength-6",
+    ];
     const bumperGroupBoxIds = [
         'box-bumper-12-1st-1',
         'box-bumper-12-2nd-1',
@@ -84,7 +100,7 @@ export default function Table(props) {
         'feature-droptarget-red-4',
         'feature-droptarget-red-56'
     ];
-    const allFeatureIds = [...ferriswheelFeatureIds, ...bumperFeatureIds, ...flipperIds, ...yelDropTargetFeatureIds, ...redDropTargetFeatureIds, 'start'];
+    const allFeatureIds = [...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, ...flipperIds, ...yelDropTargetFeatureIds, ...redDropTargetFeatureIds, 'start'];
     //#endregion
     useEffect(() => {
         for (const dashedBoxId of dashedBoxIds) {
@@ -103,72 +119,198 @@ export default function Table(props) {
     return (
         <div id="table">
             <img src="/images/carniball.jpg" alt="Carniball board" />
-            //#region ferriswheel-cars
-            //#region ferriswheel-car-12
-            <Box boxId="box-ferriswheel-car-12"
-                canReceiveOn={[1, 2]}
-                canReceiveFrom={['start', 'yel-flipper']}
-                correspondingFeatureId="feature-ferriswheel-car-12"
-                groupBoxIds={ferriswheelGroupBoxIds}
-                ball1FeatureId={props.ball1FeatureId}
-                ball2FeatureId={props.ball2FeatureId}
-                setBall1FeatureId={props.setBall1FeatureId}
-                setBall2FeatureId={props.setBall2FeatureId}
-                x="160px"
-                y="246px"
-                height="48px"
-                width="65px"
-                {...props}
-            />
-            <Feature featureId="feature-ferriswheel-car-12"
-                x="182px"
-                y="304px"
-            />
-            //#endregion
-            //#region ferriswheel-car-34
-            <Box boxId="box-ferriswheel-car-34"
-                canReceiveOn={[3, 4]}
-                canReceiveFrom={['start', 'yel-flipper']}
-                correspondingFeatureId="feature-ferriswheel-car-34"
-                groupBoxIds={ferriswheelGroupBoxIds}
-                ball1FeatureId={props.ball1FeatureId}
-                ball2FeatureId={props.ball2FeatureId}
-                setBall1FeatureId={props.setBall1FeatureId}
-                setBall2FeatureId={props.setBall2FeatureId}
-                x="255px"
-                y="230px"
-                height="40px"
-                width="62px"
-                {...props}
-            />
-            <Feature featureId="feature-ferriswheel-car-34"
-                x="274px"
-                y="280px"
-            />
-            //#endregion
-            //#region ferriswheel-car-56
-            <Box boxId="box-ferriswheel-car-56"
-                canReceiveOn={[5, 6]}
-                canReceiveFrom={['start', 'yel-flipper']}
-                correspondingFeatureId="feature-ferriswheel-car-56"
-                groupBoxIds={ferriswheelGroupBoxIds}
-                ball1FeatureId={props.ball1FeatureId}
-                ball2FeatureId={props.ball2FeatureId}
-                setBall1FeatureId={props.setBall1FeatureId}
-                setBall2FeatureId={props.setBall2FeatureId}
-                x="350px"
-                y="246px"
-                height="48px"
-                width="62px"
-                {...props}
-            />
-            <Feature featureId="feature-ferriswheel-car-56"
-                x="365px"
-                y="304px"
-            />
-            //#endregion
-            //#endregion
-            {/* bumper-12 */}
+            <React.Fragment key="ferriswheel">
+                <Box boxId="box-ferriswheel-car-12"
+                    canReceiveOn={[1, 2]}
+                    canReceiveFrom={['start', 'yel-flipper']}
+                    correspondingFeatureId="feature-ferriswheel-car-12"
+                    groupBoxIds={ferriswheelGroupBoxIds}
+                    ball1FeatureId={props.ball1FeatureId}
+                    ball2FeatureId={props.ball2FeatureId}
+                    setBall1FeatureId={props.setBall1FeatureId}
+                    setBall2FeatureId={props.setBall2FeatureId}
+                    x="160px"
+                    y="246px"
+                    height="48px"
+                    width="65px"
+                    {...props}
+                />
+                <Feature featureId="feature-ferriswheel-car-12"
+                    x="182px"
+                    y="304px"
+                />
+                <Box boxId="box-ferriswheel-car-34"
+                    canReceiveOn={[3, 4]}
+                    canReceiveFrom={['start', 'yel-flipper']}
+                    correspondingFeatureId="feature-ferriswheel-car-34"
+                    groupBoxIds={ferriswheelGroupBoxIds}
+                    ball1FeatureId={props.ball1FeatureId}
+                    ball2FeatureId={props.ball2FeatureId}
+                    setBall1FeatureId={props.setBall1FeatureId}
+                    setBall2FeatureId={props.setBall2FeatureId}
+                    x="255px"
+                    y="230px"
+                    height="40px"
+                    width="62px"
+                    {...props}
+                />
+                <Feature featureId="feature-ferriswheel-car-34"
+                    x="274px"
+                    y="280px"
+                />
+                <Box boxId="box-ferriswheel-car-56"
+                    canReceiveOn={[5, 6]}
+                    canReceiveFrom={['start', 'yel-flipper']}
+                    correspondingFeatureId="feature-ferriswheel-car-56"
+                    groupBoxIds={ferriswheelGroupBoxIds}
+                    ball1FeatureId={props.ball1FeatureId}
+                    ball2FeatureId={props.ball2FeatureId}
+                    setBall1FeatureId={props.setBall1FeatureId}
+                    setBall2FeatureId={props.setBall2FeatureId}
+                    x="350px"
+                    y="246px"
+                    height="48px"
+                    width="62px"
+                    {...props}
+                />
+                <Feature featureId="feature-ferriswheel-car-56"
+                    x="365px"
+                    y="304px"
+                />
+            </React.Fragment>
+            <React.Fragment key="featofstrength">
+                <React.Fragment key="featofstrength-1">
+                    <Feature featureId="feature-featofstrength-1"
+                        x="376px"
+                        y="535px"
+                    />
+                    <Box boxId="box-featofstrength-1"
+                        canReceiveOn={[1]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-1"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="376px"
+                        y="535px"
+                        height="25px"
+                        width="25px"
+                        {...props}
+                    />
+                </React.Fragment>
+                <React.Fragment key="featofstrength-2">
+                    <Feature featureId="feature-featofstrength-2"
+                        x="388px"
+                        y="505px"
+                    />
+                    <Box boxId="box-featofstrength-2"
+                        canReceiveOn={[2]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-2"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="388px"
+                        y="505px"
+                        height="25px"
+                        width="25px"
+                        points="1"
+                        {...props}
+                    />
+                </React.Fragment>
+                <React.Fragment key="featofstrength-3">
+                    <Feature featureId="feature-featofstrength-3"
+                        x="398px"
+                        y="477px"
+                    />
+                    <Box boxId="box-featofstrength-3"
+                        canReceiveOn={[3]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-3"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="398px"
+                        y="477px"
+                        height="25px"
+                        width="25px"
+                        points="1"
+                        {...props}
+                    />
+                </React.Fragment>
+                <React.Fragment key="featofstrength-4">
+                    <Feature featureId="feature-featofstrength-4"
+                        x="409px"
+                        y="448px"
+                    />
+                    <Box boxId="box-featofstrength-4"
+                        canReceiveOn={[4]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-4"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="409px"
+                        y="448px"
+                        height="25px"
+                        width="25px"
+                        points="2"
+                        {...props}
+                    />
+                </React.Fragment>
+                <React.Fragment key="featofstrength-5">
+                    <Feature featureId="feature-featofstrength-5"
+                        x="420px"
+                        y="420px"
+                    />
+                    <Box boxId="box-featofstrength-5"
+                        canReceiveOn={[5]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-5"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="420px"
+                        y="420px"
+                        height="25px"
+                        width="25px"
+                        points="5"
+                        {...props}
+                    />
+                </React.Fragment>
+                <React.Fragment key="featofstrength-6">
+                    <Feature featureId="feature-featofstrength-6"
+                        x="430px"
+                        y="390px"
+                    />
+                    <Box boxId="box-featofstrength-6"
+                        canReceiveOn={[6]}
+                        canReceiveFrom={["red-flipper"]}
+                        correspondingFeatureId="feature-featofstrength-6"
+                        groupBoxIds={featofstrengthGroupBoxIds}
+                        ball1FeatureId={props.ball1FeatureId}
+                        ball2FeatureId={props.ball2FeatureId}
+                        setBall1FeatureId={props.setBall1FeatureId}
+                        setBall2FeatureId={props.setBall2FeatureId}
+                        x="430px"
+                        y="390px"
+                        height="25px"
+                        width="25px"
+                        points="20"
+                        {...props}
+                    />
+                </React.Fragment>
+            </React.Fragment>
             <Box boxId="box-bumper-12-1st-1"
                 canReceiveOn={[1]}
                 canReceiveFrom={['start', 'feature-bumper-56', ...flipperIds]}
@@ -237,7 +379,6 @@ export default function Table(props) {
                 x="170px"
                 y="388px"
             />
-            {/* bumper-34 */}
             <Box boxId="box-bumper-34-1st-3"
                 canReceiveOn={[3]}
                 canReceiveFrom={['start', ...ferriswheelFeatureIds, 'feature-bumper-12', ...flipperIds]}
@@ -306,7 +447,6 @@ export default function Table(props) {
                 x="303px"
                 y="388px"
             />
-            {/* bumper-56 */}
             <Box boxId="box-bumper-56-1st-5"
                 canReceiveOn={[5]}
                 canReceiveFrom={['start', ...ferriswheelFeatureIds, 'feature-bumper-34', ...flipperIds]}
@@ -381,7 +521,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-yel-12"
                 canReceiveOn={[1, 2]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-yel-12"
                 groupBoxIds={yelDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -401,7 +541,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-yel-34"
                 canReceiveOn={[3, 4]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-yel-34"
                 groupBoxIds={yelDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -421,7 +561,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-yel-56"
                 canReceiveOn={[5, 6]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'yel-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-yel-56"
                 groupBoxIds={yelDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -441,7 +581,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-red-12"
                 canReceiveOn={[1, 2]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-red-12"
                 groupBoxIds={redDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -461,7 +601,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-red-3"
                 canReceiveOn={[3]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-red-3"
                 groupBoxIds={redDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -481,7 +621,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-red-4"
                 canReceiveOn={[4]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-red-4"
                 groupBoxIds={redDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
@@ -501,7 +641,7 @@ export default function Table(props) {
             />
             <Box boxId="box-droptarget-red-56"
                 canReceiveOn={[5, 6]}
-                canReceiveFrom={[...ferriswheelFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
+                canReceiveFrom={[...ferriswheelFeatureIds, ...featofstrengthFeatureIds, ...bumperFeatureIds, 'red-flipper', 'start']}
                 correspondingFeatureId="feature-droptarget-red-56"
                 groupBoxIds={redDropTargetGroupBoxIds}
                 ball1FeatureId={props.ball1FeatureId}
