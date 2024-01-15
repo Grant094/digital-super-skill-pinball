3fb6d8684bef61d21b401c7366fa5de0ba9eb4ff
Grant Oxer <grant.oxer@gmail.com>
2024-01-15 09:10:21 -0800

    changed Outlane.js to use constants.js instead of redefining its own constants

--- web-app/src/app/Outlane.js
+++ web-app/src/app/Outlane.js
@@ -1,26 +1,11 @@
 'use client'
 
 import React, { useState, useEffect } from "react"
+import * as constants from "./constants"
 import DashedBox from "./DashedBox";
 
 export default function Outlane(props) {
     const POINTS_PER_USED_FLIPPER_BOX = 2;
-    const redFlipperGroupBoxIds = [
-        'red-flipper-box-3',
-        'red-flipper-box-45',
-        'red-flipper-box-6'
-    ];
-    const yelFlipperGroupBoxIds = [
-                'yel-flipper-box-1',
-        'yel-flipper-box-23',
-        'yel-flipper-box-4'
-    ];
-    const inlaneBoxIds = [
-        'red-flipper-inlane-box-2',
-        'yel-flipper-inlane-box-5'
-    ];
-    const flipperGroupBoxIds = [...redFlipperGroupBoxIds, ...yelFlipperGroupBoxIds];
-    const dashedBoxIds = [...redFlipperGroupBoxIds, ...yelFlipperGroupBoxIds, ...inlaneBoxIds];
 
     function awardOutlanePoints() {
         let relevantFlipperBoxesUsed = 0;
@@ -34,7 +19,7 @@ export default function Outlane(props) {
 
 
         // end the round
-        for (const dashedBoxId of dashedBoxIds) {
+        for (const dashedBoxId of constants.dashedBoxIds) {
             document.getElementById(dashedBoxId).style.backgroundColor = 'transparent';
         }
 
