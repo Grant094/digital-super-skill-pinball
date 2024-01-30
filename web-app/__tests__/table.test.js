dbac974252b53a2897c705a66d0bf342f2f56239
Grant Oxer <grant.oxer@gmail.com>
2024-01-30 09:19:49 -0800

    got unit tests working!

--- web-app/__tests__/table.test.js
+++ web-app/__tests__/table.test.js
@@ -1,14 +1,23 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Table from "../src/app/Table";
+import * as constants from "../src/app/constants";
 
 describe("Table", () => {
-    it("renders the ball", () => {
+    it("renders ball 1", () => {
         // arrange
         render(<Table />);
         // act
-        const element = screen.getByAltText(/pinball1/i);
+        const element = screen.getByAltText(constants.BALL1_ID);
         // assert
         expect(element).toBeInTheDocument();
     });
+    it("does not render ball 3", () => {
+        // arrange
+        render(<Table />);
+        // act
+        const element = screen.queryByAltText(/ball3/i);
+        // assert
+        expect(element).toBeNull();
+    })
 });
