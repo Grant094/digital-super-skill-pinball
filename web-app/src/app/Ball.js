8b51b13bb8e2acb25580c1b0f933a1a3a9b4f180
Grant Oxer <grant.oxer@gmail.com>
2024-02-02 11:26:28 -0800

    moved logic for whether the ball is in the drain into a function of Ball comp

--- web-app/src/app/Ball.js
+++ web-app/src/app/Ball.js
@@ -7,6 +7,7 @@ import * as utilities from "./utilities";
 export default function Ball(props) {
     let ballTop, ballLeft;
     
+    //#region calc ballTop and ballLeft
     // since the first render is before the document becomes available, the balls' initial values need to be set this way
     if (typeof window === "undefined") {
         if (props.ballId === constants.BALL1_ID) {
@@ -20,6 +21,11 @@ export default function Ball(props) {
         ballTop = document.getElementById(props.ballFeatureId).style.top;
         ballLeft = document.getElementById(props.ballFeatureId).style.left;
     }
+    //#endregion
+
+    function isBallInDrain() {
+        return (props.ballFeatureId === constants.DRAIN_FEATURE_ID);
+    }
 
     return (
         <img id={props.ballId}
@@ -27,7 +33,7 @@ export default function Ball(props) {
                 position: "absolute",
                 top: ballTop,
                 left: ballLeft,
-                visibility: ((props.ballFeatureId === constants.DRAIN_FEATURE_ID)? "hidden": "visible"),
+                visibility: (isBallInDrain()? "hidden": "visible"),
             }}
             src="/images/ball.jpg"
             height="25px"
