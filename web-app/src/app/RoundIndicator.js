e628dc9145edbc92e79b9bdeb6914b41af00c8dd
Grant Oxer <grant.oxer@gmail.com>
2024-02-02 11:45:48 -0800

    moved logic for whether a round indicator should be visible into its own function

--- web-app/src/app/RoundIndicator.js
+++ web-app/src/app/RoundIndicator.js
@@ -4,13 +4,17 @@ import React, { useState, useEffect } from "react";
 import styles from "./roundindicator.module.css";
 
 export default function RoundIndicator(props) {
+    function isOnThisRoundOrLater() {
+        return (Number(props.forRound) <= Number(props.round));
+    }
+
     return (
         <p id={props.RoundIndicatorId}
             className={styles.RoundIndicator}
             style={{
                 top: props.top,
                 left: props.left,
-                visibility: (Number(props.forRound) <= Number(props.round) ? "visible": "hidden"),
+                visibility: (isOnThisRoundOrLater() ? "visible": "hidden"),
             }}
         >
             X
