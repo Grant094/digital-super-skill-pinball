ad8970f285ae6c4cb7ded21189ee61e681336f8a
Grant Oxer <grant.oxer@gmail.com>
2024-02-02 11:43:45 -0800

    changed RoundIndicator.js to calc visibility instead of storing it in state and updating via useEffect

--- web-app/src/app/RoundIndicator.js
+++ web-app/src/app/RoundIndicator.js
@@ -4,19 +4,13 @@ import React, { useState, useEffect } from "react";
 import styles from "./roundindicator.module.css";
 
 export default function RoundIndicator(props) {
-    const [roundIndicatorVisibility, setRoundIndicatorVisibility] = useState("hidden");
-
-    useEffect(function checkWhetherThisShouldBeVisible() {
-        setRoundIndicatorVisibility(Number(props.forRound) <= Number(props.round) ? "visible": "hidden");
-    }, [props.round]);
-
     return (
         <p id={props.RoundIndicatorId}
             className={styles.RoundIndicator}
             style={{
                 top: props.top,
                 left: props.left,
-                visibility: roundIndicatorVisibility,
+                visibility: (Number(props.forRound) <= Number(props.round) ? "visible": "hidden"),
             }}
         >
             X
