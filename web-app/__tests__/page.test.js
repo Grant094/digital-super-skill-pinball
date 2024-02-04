a1ed96a80ec3a8afdbf23b23dcd0b518e62355fe
Grant Oxer <grant.oxer@gmail.com>
2024-02-04 10:13:29 -0800

    removed redundant test

--- web-app/__tests__/page.test.js
+++ web-app/__tests__/page.test.js
@@ -4,14 +4,6 @@ import Home from "../src/app/page";
 import * as constants from "../src/app/constants";
 
 describe("Table", () => {
-    it("renders ball 1", () => {
-        // arrange
-        render(<Home />);
-        // act
-        const element = screen.getByAltText(constants.BALL1_ID);
-        // assert
-        expect(element).toBeInTheDocument();
-    });
     it("renders everything correctly on page load", () => {
         // arrange
         render(<Home />);
