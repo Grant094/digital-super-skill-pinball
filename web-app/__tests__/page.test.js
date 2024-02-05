f7bcbf83669b56ea5684fd99957f867747fbc206
Grant Oxer <grant.oxer@gmail.com>
2024-02-05 14:41:39 -0800

    deleted describe('on restart',...)

--- web-app/__tests__/page.test.js
+++ web-app/__tests__/page.test.js
@@ -59,69 +59,6 @@ describe("Home", () => {
             `);
         });
     });
-    describe('on restart', () => {
-        it.each(constants.ALL_FEATURE_IDS)('should render %s as visible',
-            async (featureId) => {
-                // arrange
-                const user = userEvent.setup();
-                render(<Home />);
-                // act
-                await user.click(screen.getByTitle(constants.RESTART_BUTTON_ID));
-                const element = screen.getByTitle(featureId);
-                // assert
-                expect(element).toBeInTheDocument()
-                expect(element).toHaveStyle("visibility: visible");
-            }
-        );
-        it.each(constants.ALL_BOX_IDS)('should render %s as visible and unfilled',
-            async (boxId) => {
-                // arrange
-                const user = userEvent.setup();
-                render(<Home />);
-                // act
-                await user.click(screen.getByTitle(constants.RESTART_BUTTON_ID));
-                const element = screen.getByTitle(boxId);
-                // assert
-                expect(element).toBeInTheDocument();
-                expect(element).toHaveStyle(`
-                    visibility: visible;
-                    backgroundColor: ${constants.UNFILLED_BACKGROUND_COLOR};
-                `);
-            }
-        );
-        it('should render ball1 as visible at the Start feature', async () => {
-            // arrange
-            const user = userEvent.setup();
-            render(<Home />);
-            // act
-            await user.click(screen.getByTitle(constants.RESTART_BUTTON_ID));
-            const ball1Element = screen.getByTitle(constants.BALL1_ID);
-            const startFeatureElement = screen.getByTitle(constants.START_FEATURE_ID);
-            // assert
-            expect(ball1Element).toBeInTheDocument();
-            expect(ball1Element).toHaveStyle(`
-                visibility: visible;
-                top: ${startFeatureElement.style.top};
-                left: ${startFeatureElement.style.left};
-            `);
-        });
-        it('should render ball2 as hidden at the Drain feature', async () => {
-            // arrange
-            const user = userEvent.setup();
-            render(<Home />);
-            // act
-            await user.click(screen.getByTitle(constants.RESTART_BUTTON_ID));
-            const ball2Element = screen.getByTitle(constants.BALL2_ID);
-            const drainFeatureElement = screen.getByTitle(constants.DRAIN_FEATURE_ID);
-            // assert
-            expect(ball2Element).toBeInTheDocument();
-            expect(ball2Element).toHaveStyle(`
-                visibility: hidden;
-                top: ${drainFeatureElement.style.top};
-                left: ${drainFeatureElement.style.left};
-            `);
-        });
-    })
     describe('inputting specific dice rolls', () => {
         it('should display the 1st dice values passed via props', async () => {
             // arrange
