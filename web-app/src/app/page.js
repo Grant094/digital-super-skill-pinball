dc090ad9a3289195c45325fb7e04d76b0e726908
Grant Oxer <grant.oxer@gmail.com>
2024-02-07 11:27:17 -0800

    verified I can pass die values via Home comp to Game comp for manual testing

--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -4,6 +4,10 @@ import React, { useState, useEffect } from "react";
 import Game from "./Game";
 
 export default function Home(props) {
+  const DIE_VALUES = [
+    [1, 1], // final roll
+  ];
+
   return (
     <div>
       <Game />
