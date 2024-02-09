10285d3a7b8aa3521fb85538dca9ce0024a4b8af
Grant Oxer <grant.oxer@gmail.com>
2024-02-09 14:07:12 -0800

    aligned indentation of page.js and Game.js with that of other files

--- web-app/src/app/Game.js
+++ web-app/src/app/Game.js
@@ -9,169 +9,169 @@ import ScoreIndicator from "./ScoreIndicator";
 import RestartTray from "./RestartTray";
 
 export default function Game(props) {
-  let didInit = false;
-  //#region state
-  const [dieValuesIndex, setDieValuesIndex] = useState(props.dieValues ? 0 : null);
-  const [die1, setDie1] = useState(props.dieValues ? props.dieValues[dieValuesIndex] : 0);
-  const [die2, setDie2] = useState(props.dieValues ? props.dieValues[dieValuesIndex] : 0);
-  const [die1AmountNudgedBy, setDie1AmountNudgedBy] = useState(0);
-  const [die2AmountNudgedBy, setDie2AmountNudgedBy] = useState(0);
-  const [nudgesUsed, setNudgesUsed] = useState(0);
-  const [score, setScore] = useState(0);
-  const [round, setRound] = useState(1);
-  const [ball1FeatureId, setBall1FeatureId] = useState(constants.START_FEATURE_ID);
-  const [ball2FeatureId, setBall2FeatureId] = useState(constants.DRAIN_FEATURE_ID);
-  const [selectedBallId, setSelectedBallId] = useState(constants.BALL1_ID);
-  //#endregion
+    let didInit = false;
+    //#region state
+    const [dieValuesIndex, setDieValuesIndex] = useState(props.dieValues ? 0 : null);
+    const [die1, setDie1] = useState(props.dieValues ? props.dieValues[dieValuesIndex] : 0);
+    const [die2, setDie2] = useState(props.dieValues ? props.dieValues[dieValuesIndex] : 0);
+    const [die1AmountNudgedBy, setDie1AmountNudgedBy] = useState(0);
+    const [die2AmountNudgedBy, setDie2AmountNudgedBy] = useState(0);
+    const [nudgesUsed, setNudgesUsed] = useState(0);
+    const [score, setScore] = useState(0);
+    const [round, setRound] = useState(1);
+    const [ball1FeatureId, setBall1FeatureId] = useState(constants.START_FEATURE_ID);
+    const [ball2FeatureId, setBall2FeatureId] = useState(constants.DRAIN_FEATURE_ID);
+    const [selectedBallId, setSelectedBallId] = useState(constants.BALL1_ID);
+    //#endregion
 
-  //#region functions
-  const incNudgesUsed = (() => setNudgesUsed(nudgesUsed + 1));
-  const incRound = (() => setRound(round + 1));
+    //#region functions
+    const incNudgesUsed = (() => setNudgesUsed(nudgesUsed + 1));
+    const incRound = (() => setRound(round + 1));
 
-  function getSelectedBallFeatureId(selectedBallId) {
-    if (selectedBallId === constants.BALL1_ID) {
-      return ball1FeatureId;
-    } else if (selectedBallId === constants.BALL2_ID) {
-      return ball2FeatureId;
-    } else {
-      return null;
-    }
-  }
-
-  function moveSelectedBall(correspondingFeatureId) {
-    if (selectedBallId === constants.BALL1_ID) {
-      setBall1FeatureId(correspondingFeatureId);
-    } else if (selectedBallId === constants.BALL2_ID) {
-      setBall2FeatureId(correspondingFeatureId);
-    }
-  }
-
-  function endRound(clearDashedBoxes = null) {
-    // increment round
-    incRound();
-
-    if (clearDashedBoxes) {
-        clearDashedBoxes();
+    function getSelectedBallFeatureId(selectedBallId) {
+        if (selectedBallId === constants.BALL1_ID) {
+            return ball1FeatureId;
+        } else if (selectedBallId === constants.BALL2_ID) {
+            return ball2FeatureId;
+        } else {
+            return null;
+        }
     }
 
-    // move ball1 to start
-    setSelectedBallId(constants.BALL1_ID);
-    moveSelectedBall(constants.START_FEATURE_ID);
-  }
-
-  function rollDice() {
-    if (props.dieValues) {
-      setDieValuesIndex(() => dieValuesIndex + 1);
+    function moveSelectedBall(correspondingFeatureId) {
+        if (selectedBallId === constants.BALL1_ID) {
+            setBall1FeatureId(correspondingFeatureId);
+        } else if (selectedBallId === constants.BALL2_ID) {
+            setBall2FeatureId(correspondingFeatureId);
+        }
     }
-    const nextValueOfDie1 = props.dieValues ? props.dieValues[dieValuesIndex][0] : utilities.getRndIntegerInclusive(1, 6);
-    const nextValueOfDie2 = props.dieValues ? props.dieValues[dieValuesIndex][1] : utilities.getRndIntegerInclusive(1, 6);
-    setDie1(nextValueOfDie1);
-    setDie2(nextValueOfDie2);
 
-    if (utilities.calcNetNudgeAmount(die1AmountNudgedBy, die2AmountNudgedBy)) {
-      // check whether player tilted and if so end the round
-      if (utilities.calcNetNudgeAmount(die1AmountNudgedBy, die2AmountNudgedBy) > Math.abs(nextValueOfDie1 - nextValueOfDie2)) {
-        // player tilted so they should be notified and then the round should be ended
-        alert(`Tilted!`);
+    function endRound(clearDashedBoxes = null) {
+        // increment round
+        incRound();
 
-        endRound();
-      }
+        if (clearDashedBoxes) {
+            clearDashedBoxes();
+        }
 
-      // after checking tilt status, remove any nudging from both dice
-      setDie1AmountNudgedBy(0);
-      setDie2AmountNudgedBy(0);
+        // move ball1 to start
+        setSelectedBallId(constants.BALL1_ID);
+        moveSelectedBall(constants.START_FEATURE_ID);
     }
-  }
 
-  function handleNudge(dieId, symbol) {
-    if (dieId === "1") {
-      if (symbol === "+") {
-        setDie1(Number(die1) + 1);
-        setDie1AmountNudgedBy(Number(die1AmountNudgedBy) + 1);
-      } else {
-        setDie1(Number(die1) - 1);
-        setDie1AmountNudgedBy(Number(die1AmountNudgedBy) - 1);
-      }
-    } else {
-      if (symbol === "+") {
-        setDie2(Number(die2) + 1);
-        setDie2AmountNudgedBy(Number(die2AmountNudgedBy) + 1);
-      } else {
-        setDie2(Number(die2) - 1);
-        setDie2AmountNudgedBy(Number(die2AmountNudgedBy) - 1);
-      }
+    function rollDice() {
+        if (props.dieValues) {
+            setDieValuesIndex(() => dieValuesIndex + 1);
+        }
+        const nextValueOfDie1 = props.dieValues ? props.dieValues[dieValuesIndex][0] : utilities.getRndIntegerInclusive(1, 6);
+        const nextValueOfDie2 = props.dieValues ? props.dieValues[dieValuesIndex][1] : utilities.getRndIntegerInclusive(1, 6);
+        setDie1(nextValueOfDie1);
+        setDie2(nextValueOfDie2);
+
+        if (utilities.calcNetNudgeAmount(die1AmountNudgedBy, die2AmountNudgedBy)) {
+            // check whether player tilted and if so end the round
+            if (utilities.calcNetNudgeAmount(die1AmountNudgedBy, die2AmountNudgedBy) > Math.abs(nextValueOfDie1 - nextValueOfDie2)) {
+                // player tilted so they should be notified and then the round should be ended
+                alert(`Tilted!`);
+
+                endRound();
+            }
+
+            // after checking tilt status, remove any nudging from both dice
+            setDie1AmountNudgedBy(0);
+            setDie2AmountNudgedBy(0);
+        }
     }
-  }
 
-  function handleRestart() {
-    props.incGameId();
-  }
-
-  function addPoints(pointsToAdd) {
-    setScore(Number(score) + Number(pointsToAdd));
-  }
-
-  function autoSelectOnlyRemainingBall() {
-    if (ball1FeatureId === constants.DRAIN_FEATURE_ID && ball2FeatureId !== constants.DRAIN_FEATURE_ID) {
-      setSelectedBallId(constants.BALL2_ID);
-    } else if (ball2FeatureId === constants.DRAIN_FEATURE_ID && ball1FeatureId !== constants.DRAIN_FEATURE_ID) {
-      setSelectedBallId(constants.BALL1_ID);
+    function handleNudge(dieId, symbol) {
+        if (dieId === "1") {
+            if (symbol === "+") {
+                setDie1(Number(die1) + 1);
+                setDie1AmountNudgedBy(Number(die1AmountNudgedBy) + 1);
+            } else {
+                setDie1(Number(die1) - 1);
+                setDie1AmountNudgedBy(Number(die1AmountNudgedBy) - 1);
+            }
+        } else {
+            if (symbol === "+") {
+                setDie2(Number(die2) + 1);
+                setDie2AmountNudgedBy(Number(die2AmountNudgedBy) + 1);
+            } else {
+                setDie2(Number(die2) - 1);
+                setDie2AmountNudgedBy(Number(die2AmountNudgedBy) - 1);
+            }
+        }
     }
-  }
 
-  function gameOverAlert() {
-    alert(`Game over!`);
-  }
-  //#endregion
-
-  //#region useEffect
-  useEffect(function init() {
-    if (!didInit) {
-      didInit = true;
-      rollDice();
+    function handleRestart() {
+        props.incGameId();
     }
-  }, []);
-  //#endregion
 
-  return (
-    <div>
-      <Table tableId="table"
-        die1={die1}
-        die2={die2}
-        rollDice={rollDice}
-        score={score}
-        round={round}
-        addPoints={addPoints}
-        ball1FeatureId={ball1FeatureId}
-        ball2FeatureId={ball2FeatureId}
-        die1AmountNudgedBy={die1AmountNudgedBy}
-        die2AmountNudgedBy={die2AmountNudgedBy}
-        nudgesUsed={nudgesUsed}
-        incNudgesUsed={incNudgesUsed}
-        selectedBallId={selectedBallId}
-        getSelectedBallFeatureId={() => getSelectedBallFeatureId(selectedBallId)}
-        moveSelectedBall={moveSelectedBall}
-        endRound={endRound}
-        autoSelectOnlyRemainingBall={autoSelectOnlyRemainingBall}
-        gameOverAlert={gameOverAlert}
-      />
-      <DiceTray dicetrayId="dice-tray"
-        die1={die1}
-        die2={die2}
-        die1AmountNudgedBy={die1AmountNudgedBy}
-        die2AmountNudgedBy={die2AmountNudgedBy}
-        nudgesUsed={nudgesUsed}
-        onNudge={handleNudge}
-        rollDice={rollDice}
-      />
-      <ScoreIndicator scoreIndicatorId={constants.SCORE_INDICATOR_ID}
-        scorePId={constants.SCORE_PARAGRAPH_ID}
-        score={score}
-      />
-      <RestartTray restartrayId="restart-tray"
-        restartButtonId={constants.RESTART_BUTTON_ID}
-        onClick={handleRestart}
-      />
-    </div>
-  );
+    function addPoints(pointsToAdd) {
+        setScore(Number(score) + Number(pointsToAdd));
+    }
+
+    function autoSelectOnlyRemainingBall() {
+        if (ball1FeatureId === constants.DRAIN_FEATURE_ID && ball2FeatureId !== constants.DRAIN_FEATURE_ID) {
+            setSelectedBallId(constants.BALL2_ID);
+        } else if (ball2FeatureId === constants.DRAIN_FEATURE_ID && ball1FeatureId !== constants.DRAIN_FEATURE_ID) {
+            setSelectedBallId(constants.BALL1_ID);
+        }
+    }
+
+    function gameOverAlert() {
+        alert(`Game over!`);
+    }
+    //#endregion
+
+    //#region useEffect
+    useEffect(function init() {
+        if (!didInit) {
+            didInit = true;
+            rollDice();
+        }
+    }, []);
+    //#endregion
+
+    return (
+        <div>
+            <Table tableId="table"
+                die1={die1}
+                die2={die2}
+                rollDice={rollDice}
+                score={score}
+                round={round}
+                addPoints={addPoints}
+                ball1FeatureId={ball1FeatureId}
+                ball2FeatureId={ball2FeatureId}
+                die1AmountNudgedBy={die1AmountNudgedBy}
+                die2AmountNudgedBy={die2AmountNudgedBy}
+                nudgesUsed={nudgesUsed}
+                incNudgesUsed={incNudgesUsed}
+                selectedBallId={selectedBallId}
+                getSelectedBallFeatureId={() => getSelectedBallFeatureId(selectedBallId)}
+                moveSelectedBall={moveSelectedBall}
+                endRound={endRound}
+                autoSelectOnlyRemainingBall={autoSelectOnlyRemainingBall}
+                gameOverAlert={gameOverAlert}
+            />
+            <DiceTray dicetrayId="dice-tray"
+                die1={die1}
+                die2={die2}
+                die1AmountNudgedBy={die1AmountNudgedBy}
+                die2AmountNudgedBy={die2AmountNudgedBy}
+                nudgesUsed={nudgesUsed}
+                onNudge={handleNudge}
+                rollDice={rollDice}
+            />
+            <ScoreIndicator scoreIndicatorId={constants.SCORE_INDICATOR_ID}
+                scorePId={constants.SCORE_PARAGRAPH_ID}
+                score={score}
+            />
+            <RestartTray restartrayId="restart-tray"
+                restartButtonId={constants.RESTART_BUTTON_ID}
+                onClick={handleRestart}
+            />
+        </div>
+    );
 }
--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -4,32 +4,32 @@ import React, { useState } from "react";
 import Game from "./Game";
 
 export default function Home(props) {
-  //#region state
-  const [gameId, setGameId] = useState(0);
-  //#endregion
+    //#region state
+    const [gameId, setGameId] = useState(0);
+    //#endregion
 
-  //#region vars
-  const MANUAL_DIE_VALUES = [
-    [1, 1], // final roll
-  ];
-  //#endregion
+    //#region vars
+    const MANUAL_DIE_VALUES = [
+        [1, 1], // final roll
+    ];
+    //#endregion
 
-  //#region functions
-  const incGameId = (() => setGameId(gameId + 1));
-  //#endregion
+    //#region functions
+    const incGameId = (() => setGameId(gameId + 1));
+    //#endregion
 
-  return (
-    <div>
-      <Game 
-        key={gameId}
-        incGameId={incGameId}
-        dieValues={props.dieValues?
-            props.dieValues:
-            (MANUAL_DIE_VALUES.length > 1)?
-                MANUAL_DIE_VALUES:
-                null
-        }
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Game
+                key={gameId}
+                incGameId={incGameId}
+                dieValues={props.dieValues ?
+                    props.dieValues :
+                    (MANUAL_DIE_VALUES.length > 1) ?
+                        MANUAL_DIE_VALUES :
+                        null
+                }
+            />
+        </div>
+    );
 }
