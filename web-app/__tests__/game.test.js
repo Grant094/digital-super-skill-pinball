23e241a410f2d0aa3a217f7bfbd3b57e5716b295
Grant Oxer <grant.oxer@gmail.com>
2024-02-22 09:16:36 -0800

    wrote tests that the only remaining ball is auto-selected on rolls after multiball has been deactivated

--- web-app/__tests__/game.test.js
+++ web-app/__tests__/game.test.js
@@ -3184,6 +3184,76 @@ describe("Game", () => {
             });
         });
     });
+    describe('after multiball is deactivated', () => {
+        it('should resume auto-selecting the only remaining ball, ball1', async () => {
+            //#region arrange
+            const DIE_VALUES = [
+                [1, 2], // move from start to yel droptarget 12
+                [1, 1], // move to yel flipper via yel flipper box 1
+                [3, 4], // move to yel droptarget 34
+                [2, 3], // move to yel flipper via yel flipper box 23
+                [5, 6], // move to yel droptarget 56
+                // select yel multiball bonus
+                // select ball 2
+                [1, 6], // move ball 2 to the drain via the drain box
+                // select ball 1
+                [3, 3], // move ball 1 to red flipper via red flipper box 3
+                [1, 1], // final roll
+            ];
+            const user = userEvent.setup();
+            render(<Game dieValues={DIE_VALUES} />);
+            //#endregion
+            //#region act
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_12_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_1_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_34_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_23_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_56_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_MULTIBALL_BONUS_BOX_ID));
+            await user.click(screen.getByTitle(constants.BALL2_ID));
+            await user.click(screen.getByTitle(constants.DRAIN_BOX_ID));
+            await user.click(screen.getByTitle(constants.BALL1_ID));
+            await user.click(screen.getByTitle(constants.RED_FLIPPER_BOX_3_BOX_ID));
+            //#endregion
+            //#region assert
+            expect(screen.getByTitle(constants.BALL1_ID).style.borderColor).toEqual(constants.BALL_SELECTED_BORDER_COLOR);
+            //#endregion
+        });
+        it('should resume auto-selecting the only remaining ball, ball2', async () => {
+            //#region arrange
+            const DIE_VALUES = [
+                [1, 2], // move from start to yel droptarget 12
+                [1, 1], // move to yel flipper via yel flipper box 1
+                [3, 4], // move to yel droptarget 34
+                [2, 3], // move to yel flipper via yel flipper box 23
+                [5, 6], // move to yel droptarget 56
+                // select yel multiball bonus
+                // select ball 1
+                [1, 6], // move ball 1 to the drain via the drain box
+                // select ball 2
+                [3, 3], // move ball 2 to red flipper via red flipper box 3
+                [1, 1], // final roll
+            ];
+            const user = userEvent.setup();
+            render(<Game dieValues={DIE_VALUES} />);
+            //#endregion
+            //#region act
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_12_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_1_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_34_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_23_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_DROPTARGET_56_BOX_ID));
+            await user.click(screen.getByTitle(constants.YEL_MULTIBALL_BONUS_BOX_ID));
+            await user.click(screen.getByTitle(constants.BALL1_ID));
+            await user.click(screen.getByTitle(constants.DRAIN_BOX_ID));
+            await user.click(screen.getByTitle(constants.BALL2_ID));
+            await user.click(screen.getByTitle(constants.RED_FLIPPER_BOX_3_BOX_ID));
+            //#endregion
+            //#region assert
+            expect(screen.getByTitle(constants.BALL2_ID).style.borderColor).toEqual(constants.BALL_SELECTED_BORDER_COLOR);
+            //#endregion
+        });
+    });
     describe('when the bumper bonus is active', () => {
         it('should award two points when moving to a bumper', async () => {
             //#region arrange
