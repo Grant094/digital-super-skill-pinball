32719b86e48125d857079b53fa401269fd26c164
Grant Oxer <grant.oxer@gmail.com>
2024-02-26 09:11:23 -0800

    alert changes to MULTIBALL_ONLY_DIE_SELECTED_ALERT after selecting a die after attempting to move having selected neither a die nor a ball

--- web-app/__tests__/game.test.js
+++ web-app/__tests__/game.test.js
@@ -3717,6 +3717,39 @@ describe("Game", () => {
                 expect(screen.getByTitle(constants.ALERT_PARAGRAPH_ID).innerHTML).toEqual(constants.MULTIBALL_NEITHER_BALL_NOR_DIE_SELECTED_ALERT);
                 //#endregion
             });
+            describe('when a die is then selected', () => {
+                it('should change the alert to be about not having selected only a ball', async () => {
+                    //#region arrange
+                    const DIE_VALUES = [
+                        [1, 2], // move from start to yel droptarget 12
+                        [1, 1], // move to yel flipper via yel flipper box 1
+                        [3, 4], // move to yel droptarget 34
+                        [2, 3], // move to yel flipper via yel flipper box 23
+                        [5, 6], // move to yel droptarget 56
+                        // select yel multiball bonus
+                        [3, 4],
+                        // attempt to move to the red flipper via red flipper box 3
+                        // select die 1 (=3)
+                        [1, 1], // final roll that should not be seen
+                    ];
+                    const user = userEvent.setup();
+                    render(<Game dieValues={DIE_VALUES} />);
+                    //#endregion
+                    //#region act
+                    await user.click(screen.getByTitle(constants.YEL_DROPTARGET_12_BOX_ID));
+                    await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_1_BOX_ID));
+                    await user.click(screen.getByTitle(constants.YEL_DROPTARGET_34_BOX_ID));
+                    await user.click(screen.getByTitle(constants.YEL_FLIPPER_BOX_23_BOX_ID));
+                    await user.click(screen.getByTitle(constants.YEL_DROPTARGET_56_BOX_ID));
+                    await user.click(screen.getByTitle(constants.YEL_MULTIBALL_BONUS_BOX_ID));
+                    await user.click(screen.getByTitle(constants.RED_FLIPPER_BOX_3_BOX_ID));
+                    await user.click(screen.getByTitle(constants.DIE1_ID));
+                    //#endregion
+                    //#region assert
+                    expect(screen.getByTitle(constants.ALERT_PARAGRAPH_ID).innerHTML).toEqual(constants.MULTIBALL_ONLY_DIE_IS_SELECTED_ALERT);
+                    //#endregion
+                });
+            });
         });
         describe('when only one ball has been moved', () => {
             it('should ignore clicks on the moved ball', async () => {
