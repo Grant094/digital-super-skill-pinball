c756dbd6452a9aad416860eda4df21e2eaf07da5
Grant Oxer <grant.oxer@gmail.com>
2024-02-27 15:10:01 -0800

    stopped passing unused props from Game to DiceTray

--- web-app/src/app/Game.js
+++ web-app/src/app/Game.js
@@ -2009,14 +2009,12 @@ export default function Game(props) {
                 handleDie1Click={() => handleDieClick(constants.DIE1_ID)}
                 handleDie2Click={() => handleDieClick(constants.DIE2_ID)}
                 selectedDieId={selectedDieId}
-                setSelectedDieId={setSelectedDieId}
                 wasDie1UsedThisTurn={wasDie1UsedThisTurn}
                 wasDie2UsedThisTurn={wasDie2UsedThisTurn}
                 die1AmountNudgedBy={die1AmountNudgedBy}
                 die2AmountNudgedBy={die2AmountNudgedBy}
                 nudgesUsed={nudgesUsed}
                 handleNudge={handleNudge}
-                rollDice={rollDice}
             />
             <ScoreIndicator scoreIndicatorId={constants.SCORE_INDICATOR_ID}
                 scorePId={constants.SCORE_PARAGRAPH_ID}
