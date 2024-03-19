3aebbfb658bca5298ee52fc7c5c67cf53c9dcff5
Grant Oxer <grant.oxer@gmail.com>
2024-03-19 15:11:50 -0700

    improved test 'should allow multiball to be activated again'

--- web-app/__tests__/game.test.js
+++ web-app/__tests__/game.test.js
@@ -4878,22 +4878,23 @@ describe("Game", () => {
                         // select red multiball bonus
                         [1, 2],
                         // move ball 1 with die 1 (=1) from red drop target 56 to yel flipper box 1
-                        // move ball 2 with die 2 (=2) from start to bumper 12 via 1st 2 box
+                        // move ball 2 with die 2 (=2) from start to bumper 12 1st 2 box
                         [1, 3],
                         // move ball 1 with die 1 (=1) from yel flipper to yel drop target 12
-                        // move ball 2 with die 2 (=3) from bumper 12 to bumper 34 via 1st 3 box
+                        // move ball 2 with die 2 (=3) from bumper 12 to bumper 34 1st 3 box
                         [2, 5],
                         // move ball 1 with die 1 (=2) from yel drop target 12 to yel flipper box 23
-                        // move ball 2 with die 2 (=5) from bumper 34 to bumper 56 via 1st 5 box
+                        // move ball 2 with die 2 (=5) from bumper 34 to bumper 56 1st 5 box
                         [3, 1],
                         // move ball 1 with die 1 (=3) from yel flipper to yel drop target 34
-                        // move ball 2 with die 2 (=1) from bumper 56 to bumper 12 via 1st 1 box
+                        // move ball 2 with die 2 (=1) from bumper 56 to bumper 12 1st 1 box
                         [4, 4],
                         // move ball 1 with die 1 (=4) from yel drop target 34 to yel flipper box 4
-                        // move ball 2 with die 2 (=4) from bumper 12 to bumper 34 via 1st 4 box
+                        // move ball 2 with die 2 (=4) from bumper 12 to bumper 34 1st 4 box
                         [5, 6],
-                        // move ball 2 with die 2 (=6) from bumper 34 to the drain via the drain box
+                        // move ball 2 with die 2 (=6) from bumper 34 to the drain box
                         // move ball 1 with die 1 (=5) from yel flipper to yel drop target 56
+                        // select yel multiball bonus
                         [1, 1], // final roll
                     ];
                     const user = userEvent.setup();
