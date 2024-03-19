e033f3e9676e7b1c3b1e6cf7c2bf3e810ab16bab
Grant Oxer <grant.oxer@gmail.com>
2024-03-19 15:41:55 -0700

    improved tests under 'when the bumper bonus has been deactivated'

--- web-app/__tests__/game.test.js
+++ web-app/__tests__/game.test.js
@@ -5392,15 +5392,16 @@ describe("Game", () => {
         it('should award one point when moving to a bumper', async () => {
             //#region arrange
             const DIE_VALUES = [
-                [1, 2], // move from start to red drop target 12 (+1)
+                [1, 2], // move from start to red drop target 12
                 [3, 3], // move to red flipper box 3
-                [3, 3], // move to red drop target 3 (+1)
+                [3, 3], // move to red drop target 3
                 [4, 5], // move to red flipper box 45
-                [4, 4], // move to red drop target 4 (+1)
+                [4, 4], // move to red drop target 4
                 [6, 6], // move to red flipper box 6
-                [5, 6], // move to red drop target 56 (+1)
-                [2, 3], // move to drain after choosing the bumper bonus
-                [1, 1], // move to bumper 12 via 1st 1 box (+1)
+                [5, 6], // move to red drop target 56
+                // select bumper bonus
+                [2, 3], // move to drain box
+                [1, 1], // move to bumper 12 1st 1 box
                 [1, 1], // final roll
             ];
             const user = userEvent.setup();
@@ -5427,16 +5428,16 @@ describe("Game", () => {
         it('should not move counterclockwise between bumpers', async () => {
             //#region arrange
             const DIE_VALUES = [
-                [1, 2], // move from start to red drop target 12 (+1)
+                [1, 2], // move from start to red drop target 12
                 [3, 3], // move to red flipper box 3
-                [3, 3], // move to red drop target 3 (+1)
+                [3, 3], // move to red drop target 3
                 [4, 5], // move to red flipper box 45
-                [4, 4], // move to red drop target 4 (+1)
+                [4, 4], // move to red drop target 4
                 [6, 6], // move to red flipper box 6
-                [5, 6], // move to red drop target 56 (+1)
+                [5, 6], // move to red drop target 56
                 [2, 3], // move to drain after choosing the bumper bonus
-                [1, 1], // move to bumper 12 via 1st 1 box (+1)
-                [5, 5], // attempt to move to bumper 56 via 1st 5 box
+                [1, 1], // move to bumper 12 1st 1 box
+                [5, 5], // attempt to move to bumper 56 1st 5 box
             ];
             const user = userEvent.setup();
             render(<Game dieValues={DIE_VALUES} />);
