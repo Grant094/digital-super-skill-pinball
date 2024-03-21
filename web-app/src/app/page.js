434ef68c0d5be4d6f339f2faa713f29373047009
Grant Oxer <grant.oxer@gmail.com>
2024-03-21 14:50:49 -0700

    on second move of multiball turn, invalid choice alert correctly appears when making an invalid choice

--- web-app/src/app/page.js
+++ web-app/src/app/page.js
@@ -7,6 +7,14 @@ export default function Home(props) {
     //#region state
     const [gameId, setGameId] = useState(0);
     const MANUAL_DIE_VALUES = [
+        [1, 2], // move from start to yel drop target 12
+        [1, 1], // move to yel flipper box 1
+        [3, 4], // move to yel drop target 34
+        [2, 3], // move to yel flipper box 23
+        [5, 6], // move to yel drop target 56
+        // select yel multiball bonus
+        [5, 5],
+        [3, 3],
         [1, 1], // final roll
     ];
     //#endregion
